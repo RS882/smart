d5c037e3288bf2343c74f37abc733c79664afde0
Russen <russen333@gmail.com>
2022-04-13 18:06:06 +0300

    69 - firct screen continue

--- src/components/Main/FirstScreen/FirstScreen.tsx
+++ src/components/Main/FirstScreen/FirstScreen.tsx
@@ -4,13 +4,15 @@ import MenuContainer from './MenuContainer';
 
 const StyledGrid = styled.div`
 	display:grid;
-
+	
+border: 1px solid #000;
 	@media ${props => props.theme.media?.tablet || '(min-width: 767.98px)'} {
 	
 		
 	};
 	@media ${props => props.theme.media?.desktop || '(min-width: 991.98px)'} {
 		grid-template-columns: 310px 1fr;
+		column-gap:20px;
 	};
 `;
 
