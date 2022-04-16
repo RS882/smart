72af3cd793bf405cb97e4efe5982782920063cc9
Russen <russen333@gmail.com>
2022-04-16 15:32:24 +0300

    73- refactoring animation desktop search

--- src/components/Header/DesktopSearch/DecktopSearchContainer.tsx
+++ src/components/Header/DesktopSearch/DecktopSearchContainer.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { openDesktopScearch, selectIsDesktopScearch } from '../../../redux/MenuSlice';
+import { openDesktopScearch } from '../../../redux/MenuSlice';
 import { changeIsBodyLock } from '../../../redux/ModalSlice';
 import Flex from '../../Flex';
 import Modal from '../../Modal/Modal';
@@ -8,28 +8,22 @@ import CloseMenu from '../DropDownMenu/CloseMenu';
 import Scearch from '../DropDownMenu/Scearch/Scearch';
 import Container from './../../Container';
 import { useDispatch } from 'react-redux';
-import { useAppSelector } from '../../../redux/hooks';
+
 
 
 interface StyledScearchBoxProps {
-}
+	isShow: boolean;
+};
 
 const StyledScearchBox = styled.div<StyledScearchBoxProps>`
 	position:fixed;
-	top: -90px;
-	transition:top 1s ease 0s;
+	top:${props => props.isShow ? '0' : '-80px'} ;
+	transition:top 0.3s ease 0s;
 	left: 0;
 	width: 100%;
 	z-index:400;
 	min-height: 80px;
 	background-color:${props => props.theme.color.bg.main || '#fff'};
-	animation: down 1s ease forwards;
-	@keyframes down {
-		0% { top:-90px}
-		100%{top:0px}
-	}
-
-
 `;
 
 const StyledScearchWrapper = styled(Flex)`
@@ -48,17 +42,26 @@ const StyledScearch = styled.div`
 
 const DecktopSearchContainer: FC = (props) => {
 
+	const [isShow, setIsShow] = useState(false);
 	const dispatch = useDispatch();
 
 	const closeScearch = () => {
-		dispatch(openDesktopScearch(false));
-		dispatch(changeIsBodyLock(false))
+		setIsShow(false);
 	};
 
+	const closeModal = () => {
+		if (!isShow) {
+			dispatch(openDesktopScearch(false));
+			dispatch(changeIsBodyLock(false));
+		};
+	};
 
+	useEffect(() => {
+		setIsShow(true);
+	}, [])
 	return (
 		<div>
-			<StyledScearchBox >
+			<StyledScearchBox isShow={isShow} onTransitionEnd={closeModal}>
 				<Container>
 					<StyledScearchWrapper align='flex-start'>
 						<StyledScearch>
