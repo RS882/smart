c3f915bbf5cb87b5bed9a33a3dbd70179ea68ecb
Russen <russen333@gmail.com>
2022-08-16 16:27:19 +0300

    158 - add unit text for CatalogMenuItem

--- src/components/Header/CatalogMenu/CatalogMenuItem.test.tsx
+++ src/components/Header/CatalogMenu/CatalogMenuItem.test.tsx
@@ -1,11 +1,19 @@
+import { create } from 'react-test-renderer';
 import React from 'react';
+import CatalogMenuItem from './CatalogMenuItem';
 
-describe('my beverage', () => {
-	test('is delicious', () => {
+describe('Catalog menu Item - test component', () => {
+	const component = create(<CatalogMenuItem iconClass='test' menuItem={{ test: 'test' }}
+		onClickCatalogItem={() => { }} />)
+	const instance = component.root;
 
+	test('The component is created ', () => {
+		expect(instance).not.toBeNull()
 	});
 
-	test('is not sour', () => {
-
+	test('The element with the desired class will find', () => {
+		// eslint-disable-next-line testing-library/await-async-query
+		const className = instance.findAllByType('div');
+		expect(className.filter(e => e.props.className && e.props.className.includes('test')).length).not.toBe(0)
 	});
 });
