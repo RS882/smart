bd3e1b22ca5f9ef28b3907e0f57b9bccc63e80d0
Russen <russen333@gmail.com>
2022-08-16 16:40:38 +0300

    Updates

--- static/js/559.46b8023b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksmart=self.webpackChunksmart||[]).push([[559],{9559:function(n,r,t){t.r(r);var a,e=t(168),s=(t(2791),t(5751)),u=t(184),c=s.ZP.main(a||(a=(0,e.Z)(["\n\tmargin: 150px;\n"])));r.default=function(){return(0,u.jsx)(c,{children:"cart"})}}}]);
-//# sourceMappingURL=559.46b8023b.chunk.js.map
---
+++ static/js/559.eb451ff8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksmart=self.webpackChunksmart||[]).push([[559],{9559:function(n,r,t){t.r(r);var a,e=t(168),s=(t(2791),t(5751)),c=t(184),u=s.ZP.main(a||(a=(0,e.Z)(["\n\tmargin: 150px;\n"])));r.default=function(n){return console.log(n.rrr),(0,c.jsx)(u,{children:"cart"})}}}]);
+//# sourceMappingURL=559.eb451ff8.chunk.js.map
